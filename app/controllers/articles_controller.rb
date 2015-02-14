c3698f3420ccbb6240c46515610792de7cda2fbf
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-02-14 01:46:05 -0500

    created article show pages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,23 @@
 class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show]
+
 	def index
+	end
+	
+	def show
 	end
 
 	def new
 		@article = Article.new
 	end
 
+	def update
+
+
+	end
+
 	def create
-		@article = Article.new[article_params]
+		@article = Article.new(article_params)
 		if @article.save
 			redirect_to @article
 		else
@@ -16,6 +26,10 @@ class ArticlesController < ApplicationController
 	end
 
 	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
 
 	def article_params
 		params.require(:article).permit(:title, :content)
