0eacf1cae1fba476c7e2955ea9a2f91f515ad661
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-02-15 17:32:38 -0500

    Added association between users and articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214074255) do
+ActiveRecord::Schema.define(version: 20150215221117) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
